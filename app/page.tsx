25502fa2a5d00449645f291fcfadd368c04fba78
phuy <huylp@phx-smartschool.com>
2025-12-07 14:05:59 +0700

    feat: Force static rendering for the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-static";
+
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { AboutSection } from "@/components/sections/AboutSection";
